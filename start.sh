4dc135ddf4b9fd9c7e46f3d01b6c1a91378415fe
Matheus Sproesser <matheus.sproesser@gmail.com>
2021-10-07 20:16:44 -0300

    new feature: list pr by repo

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-az login -u $USER -p $PASS
 rm -rf /root/.azure-devops/python-sdk/cache
 node ./scripts.js
--- tools/az-tools.js
+++ tools/az-tools.js
@@ -2,8 +2,7 @@ const { exec } = require('child_process');
 const {joinLists} = require('./polyfill')
 
 function queryPrFactory(azHost, azProject) {
-  const command = "az repos pr list " +
-  `--org ${azHost} -p ${azProject} --status active --creator`
+  const command = `az repos pr list --org ${azHost} -p ${azProject} --status active --creator`
   return (member) => new Promise((resolve, reject) => {
     exec(`${command} "${member}"` ,
     (error, stdout, stderr) => {
@@ -17,37 +16,62 @@ function queryPrFactory(azHost, azProject) {
   }) 
 }
 
+function vstsNormalizeFields({
+  codeReviewId, 
+  createdBy: {displayName}, 
+  repository: {name: repository},
+  reviewers, title
+}) {
+  const reviews = reviewers.map(r => r.vote)
+  return {codeReviewId, displayName, repository, reviews, title}
+}
+
+function filterResults(listDraft) {
+  return prLinks => prLinks.reduce(joinLists, []).filter(pr => !pr.isDraft || listDraft)
+}
+
+function normalize(azHost, azProject) {
+  const BASE_URL = `${azHost}${azProject}/_git`
+  return prs => prs.map(vstsNormalizeFields)
+  .map(pr => {
+    pr.url = `${BASE_URL}/${pr.repository}/pullrequest/${pr.codeReviewId}`
+    return pr
+  })
+}
+
 function vstsPrSource({azHost, azProject, listDraft, members}) {
   const queryPRs = queryPrFactory(azHost, azProject)
-  
-  function vstsNormalizeFields({
-    codeReviewId, 
-    createdBy: {displayName}, 
-    repository: {name: repository},
-    reviewers, title
-  }) {
-    const reviews = reviewers.map(r => r.vote)
-    return {codeReviewId, displayName, repository, reviews, title}
-  }
-  function filterResults(listDraft) {
-    return prLinks => prLinks.reduce(joinLists, []).filter(pr => !pr.isDraft || listDraft)
-  }
   const BASE_URL = `${azHost}${azProject}/_git`
-  function normalize(prs) {
-    return prs.map(vstsNormalizeFields)
-    .map(pr => {
-      pr.url = `${BASE_URL}/${pr.repository}/pullrequest/${pr.codeReviewId}`
-      return pr
-    })
-  }
 
   return function() {
     return Promise.all(members.map(queryPRs))
     .then(filterResults(listDraft))
-    .then(normalize)
+    .then(normalize(azHost, azProject))
   }
 }
 
+function vstsPrByRepository({azHost, azProject, listDraft, repository}) {
+  const command = `az repos pr list --org ${azHost} -p ${azProject} --status active --repository ${repository}`
+  const queryRepo = () => new Promise((resolve, reject) => {
+    exec(command ,
+    (error, stdout, stderr) => {
+        if (error !== null) {
+            console.log(`exec error: ${error} + ${stderr}`);
+            reject(error)
+        } else {
+            resolve(JSON.parse(stdout))
+        }
+    });
+  })
+  return function() {
+    return Promise.all([queryRepo()])
+    .then(filterResults(listDraft))
+    .then(normalize(azHost, azProject))
+  }
+
+}
+
 module.exports = {
-  vstsPrSource
+  vstsPrSource,
+  vstsPrByRepository
 }
